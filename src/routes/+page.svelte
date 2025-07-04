fca85befc2bbed27ec174773e3987e7895c5c40b
max <hxllfxres@gmail.com>
2025-07-04 01:48:01 +0100

    added title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="w-screen h-screen absolute items-center justify-center flex">
+        <h1 class="text-white text-8xl font-mostwasted hover:text-pink-200
+        hover:scale-105 duration-200 blur-sm hover:blur-none hover:cursor-default">mascararivers</h1>
+</div>
+<div class="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.1),rgba(255,255,255,0))]"></div>
+
+<style>
+    :root {
+        background-color: black;
+    }
+</style>
