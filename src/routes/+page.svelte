405384da355f0c014e48e870b0da092560dd1add
max <hxllfxres@gmail.com>
2025-07-05 02:50:05 +0100

    Added basic social links

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,29 @@
 <div class="w-screen h-screen absolute items-center justify-center flex">
+    <div class="group flex flex-col justify-center items-center">
         <h1 class="text-white text-8xl font-mostwasted hover:text-pink-200
-        hover:scale-105 duration-200 blur-sm hover:blur-none hover:cursor-default">mascararivers</h1>
+        hover:scale-105 duration-200 blur-sm hover:blur-none hover:cursor-default
+        animate-fade-down">mascararivers</h1>
+
+        <div class="flex flex-row gap-10 opacity-0 group-hover:opacity-100 group-hover:mt-10
+        duration-200 justify-center items-center text-center">
+            <h1 id="link"><a href="https://github.com/mascararivers">GitHub</a></h1>
+            <h1 id="link"><a href="https://namemc.com/profile/mascararivers">NameMC</a></h1>
+            <h1 id="link">Discord</h1>
+            <h1 id="link"><a href="projects">Projects</a></h1>
+            <h1 id="link"><a href="about">About Me</a></h1>
+        </div>
+
+    </div>
 </div>
+
 <div class="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.1),rgba(255,255,255,0))]"></div>
 
 <style>
+    @reference '../app.css';
     :root {
         background-color: black;
+    }
+    #link {
+        @apply text-white text-4xl font-mostwasted cursor-default blur-sm hover:blur-none hover:scale-125 duration-300;
     }
 </style>
